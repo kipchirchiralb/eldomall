7e0a25e387b351bb9c8b0d3e275be52e8ad09887
albert kipchirchir <albertkipchirchir02@gmail.com>
2023-04-13 15:50:11 +0300

    app structured

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.listen(3003, () => console.log("App running")); // starting the app
